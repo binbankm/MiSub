93c9b81133e450c42d84ec641f7f2d57a07a39de
binbankm <393439956@qq.com>
2025-08-02 22:22:08 +0800

    优化ProfileModal组件的输入框样式，采用现代化设计，提升用户界面的视觉效果和一致性。

--- src/components/ProfileModal.vue
+++ src/components/ProfileModal.vue
@@ -306,7 +306,7 @@ const handleDeselectAll = (listName, sourceArray) => {
                   type="text"
                   v-model="nodeSearchTerm"
                   placeholder="搜索节点..."
-                  class="w-full pl-9 pr-3 py-1.5 text-sm bg-white dark:bg-gray-800 border border-gray-300 dark:border-gray-600 rounded-md shadow-sm focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
+                  class="input-modern w-full pl-9"
                 />
                 <svg class="absolute left-3 top-1/2 -translate-y-1/2 h-4 w-4 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
               </div>
